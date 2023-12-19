dd53adf504bf34042db33ce756594a5c8fce7bfe
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2023-12-19 08:32:43 -0300

    refactor(infrastructure): change index.ts

--- source/infrastructure/database/mysql/book/index.ts
+++ source/infrastructure/database/mysql/book/index.ts
@@ -1,1 +1,20 @@
-export * from './book-repository'
+import { type DeleteBookRepository, type CreateBookRepository, type UpdateBookRepository, type FindAllBooksRepository, type FindBooksByAuthorRepository, type FindBookByIsbnRepository } from '../../../../application/ports'
+import { BookRepository } from './book-repository'
+
+const bookMySqlRepository = new BookRepository()
+
+const createBookRepository: CreateBookRepository = bookMySqlRepository
+const deleteBookRepository: DeleteBookRepository = bookMySqlRepository
+const updateBookRepository: UpdateBookRepository = bookMySqlRepository
+const findAllBooksRepository: FindAllBooksRepository = bookMySqlRepository
+const findBooksByAuthorRepository: FindBooksByAuthorRepository = bookMySqlRepository
+const findBookByIsbnRepository: FindBookByIsbnRepository = bookMySqlRepository
+
+export {
+  createBookRepository,
+  deleteBookRepository,
+  updateBookRepository,
+  findAllBooksRepository,
+  findBooksByAuthorRepository,
+  findBookByIsbnRepository
+}
